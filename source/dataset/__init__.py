3c6a3cb546503edc8c8969c5618f943f15a08b53
AnushreeBannadabhavi <anushree.bannadabhavi@gmail.com>
2023-03-16 22:10:47 -0700

    removed abcd

--- source/dataset/__init__.py
+++ source/dataset/__init__.py
@@ -1,5 +1,4 @@
 from omegaconf import DictConfig, open_dict
-from .abcd import load_abcd_data
 from .abide import load_abide_data
 from .dataloader import init_dataloader, init_stratified_dataloader
 from typing import List
@@ -8,7 +7,7 @@ import torch.utils as utils
 
 def dataset_factory(cfg: DictConfig) -> List[utils.data.DataLoader]:
 
-    assert cfg.dataset.name in ['abcd', 'abide']
+    assert cfg.dataset.name in ['abide']
 
     datasets = eval(
         f"load_{cfg.dataset.name}_data")(cfg)
